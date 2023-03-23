dcd4d6c4c6c049af64eb7bc0af6010eb853ff16a
boytsovau <alexbonch@gmail.com>
2023-03-23 17:06:32 +0300

    modified:   brain_games/scripts/brain_even.py 	modified:   dist/hexlet_code-0.1.1-py3-none-any.whl 	modified:   dist/hexlet_code-0.1.1.tar.gz

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -7,12 +7,12 @@ def main():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print("'Answer \"yes\" if the number is even, otherwise answer \"no\".")
+    print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     count = 0
     while count < 3:
         number = random.randint(1, 100)
         print(f'Question: {number}')
-        answer = prompt.string("'Your answer: ")
+        answer = prompt.string("Your answer: ")
         if (answer == "yes" and number % 2 == 0) or (answer == "no" and number % 2 != 0):
             print("Correct!")
             count += 1
