e9c35f110d6e3248d1d6ce8ebeceab5fb0eb9c87
boytsovau <alexbonch@gmail.com>
2023-03-28 17:24:58 +0300

    modified:   brain_games/scripts/brain_prime.py 	modified:   brain_games/games/prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from brain_games.welcome import welcome
+from brain_games.games.check import check_answer
+from brain_games.games.prime import question, result
+
+
+def main():
+    name = welcome()
+    print("Answer \"yes\" if given number is prime. Otherwise answer \"no\".")
+    count = 0
+    while count < 3:
+        number, user_answer = question()
+        answer = result(number)
+        if check_answer(answer, user_answer, name) is True:
+            count += 1
+        else:
+            break
+    if count == 3:
+        print(f'Congratulations, {name}')
+
+
+if __name__ == '__main__':
+    main()
