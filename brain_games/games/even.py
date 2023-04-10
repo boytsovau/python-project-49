a08652802b30fcfdb04604736201063ab8d96ff7
boytsovau <alexbonch@gmail.com>
2023-04-10 17:13:56 +0300

    modified:   brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,5 +7,5 @@ RULE = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 def get_question_and_answer():
     random_number = random.randint(1, 100)
     if not random_number % 2:
-        return f'Question: {random_number}', 'yes'
+        return random_number, 'yes'
     return random_number, 'no'
